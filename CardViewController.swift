09ae19bfdb08923d36afed5a54be2bd053b03a01
Steven Sherry <affinityforapps@gmail.com>
2016-12-12 05:09:51 -0600

    Can create card objects and pull from core data

---
+++ CardViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CardViewController.swift
+//  MTGCollector
+//
+//  Created by Steven Sherry on 12/12/16.
+//  Copyright © 2016 Affinity for Apps. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        cardImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let card = Card(context: context)
+        card.name = titleTextField.text
+        card.image = UIImagePNGRepresentation(cardImageView.image!) as NSData?
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
+
+}
