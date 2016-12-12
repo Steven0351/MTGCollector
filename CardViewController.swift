a5854c30ad9d38c529376b0031626846fb02dd79
Steven Sherry <affinityforapps@gmail.com>
2016-12-12 05:44:24 -0600

    added update and delete functionality

--- CardViewController.swift
+++ CardViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class CardViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var addUpdateButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var cardImageView: UIImageView!
     
     var imagePicker = UIImagePickerController()
+    var card : Card? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        if card != nil{
+            cardImageView.image = UIImage(data: card!.image as! Data)
+            titleTextField.text = card!.name
+            addUpdateButton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
         // Do any additional setup after loading the view.
     }
     @IBAction func photosTapped(_ sender: Any) {
@@ -40,15 +50,28 @@ class CardViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
     @IBAction func addTapped(_ sender: Any) {
         
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let card = Card(context: context)
-        card.name = titleTextField.text
-        card.image = UIImagePNGRepresentation(cardImageView.image!) as NSData?
+        if card != nil{
+            card!.name = titleTextField.text
+            card!.image = UIImagePNGRepresentation(cardImageView.image!) as NSData?
+        } else {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let card = Card(context: context)
+            card.name = titleTextField.text
+            card.image = UIImagePNGRepresentation(cardImageView.image!) as NSData?
+                    }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         navigationController?.popViewController(animated: true)
+
         
     }
 
+    @IBAction func deleteButton(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(card!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController?.popViewController(animated: true)
+    }
 }
